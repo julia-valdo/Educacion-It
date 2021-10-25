72210792d90e109c346eebc9188079610a412608
julia-valdo <71948495+julia-valdo@users.noreply.github.com>
2021-10-25 16:18:19 -0300

    Add files via upload

---
+++ Programación Web.NET con C# .NET/Base de Datos/SLNSistemaMedicos/Datos/Servidor/AdminDB.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace Datos.Servidor
+{
+    internal static class AdminDB
+    {
+        internal static SqlConnection ConectarBase()
+        {
+            string cadena = Datos.Properties.Settings.Default.KeyBDMedicos;
+            SqlConnection connection = new SqlConnection(cadena);
+            connection.Open();
+            return connection;
+        }
+    }
+}
